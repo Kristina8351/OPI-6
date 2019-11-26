a5cb77193168f8ef61fe406f3d6719a8dc3dbf85
Kristina8351 <smayl8351@gmail.com>
2019-11-26 21:35:35 +0200

    Початковий код

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include "windows.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, m;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int A[100][100];
+	cout << "Введіть розміри масиву: ";
+	cin >> m >> n;
+
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << "Введіть елемент масиву: ";
+			cin >> A[i][j];
+		}
+	}
+
+	for (int i = m; i >= 0; i--)
+	{
+		cout << endl;
+		for (int j = n; j >= 0; j--)
+		{
+			cout << A[i][j] << "  ";
+		}
+	}
+
+	return(0);
+}
